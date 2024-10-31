4ab3f2bacfec56910ab864f3dcc2f06e73a0f891
1155194577 <1155194577@link.cuhk.edu.hk>
2024-10-31 22:35:10 +0800

    test

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,4 +5,4 @@ import os
 load_dotenv()
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=int(os.getenv("PORT")) or 8000,reload=True)
-    print("Server is running on ports: ",os.getenv("PORT"))
+    print("Server is running on port: ",os.getenv("PORT"))
