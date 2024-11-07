31ddebf654faa5767831e361f7cbae48469f5234
1155194577 <1155194577@link.cuhk.edu.hk>
2024-11-07 22:48:49 +0800

    added cors setting

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -3,7 +3,15 @@ from app.api.routes.vector import vector_api_router
 from app.api.routes.indexes import indexes_api_router   
 from app.api.routes.aws import aws_api_router
 from app.api.routes.embedding import embedding_api_router
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(vector_api_router)
 app.include_router(indexes_api_router)
 app.include_router(aws_api_router)
