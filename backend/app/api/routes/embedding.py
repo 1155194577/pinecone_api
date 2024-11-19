fb2c788dd223e5863232a265547f508f817ea99f
1155194577 <111619643+1155194577@users.noreply.github.com>
2024-11-19 15:55:29 +0000

    changed to min max embedder

--- backend/app/api/routes/embedding.py
+++ backend/app/api/routes/embedding.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 from app.api.status import status_code,status_message
 from pydantic import BaseModel
 from typing import List
+from pydub import AudioSegment
 import os
 import librosa
 from app.api.embedder import MusicEmbedder,Music
@@ -12,6 +13,21 @@ embedding_api_router = APIRouter(prefix="/api/v1/embedding", tags=["embedding"])
 class EmbeddingResponse(BaseModel):
     embedding: list[float]
 
+def trim_audio(file_path, duration):
+    """Trim the audio file to the first 'duration' seconds."""
+    # Load the audio file
+    print("a")
+    audio = AudioSegment.from_file(file_path)
+
+    # Trim to the first 'duration' seconds
+    trimmed_audio = audio[:duration * 1000]  # duration in milliseconds
+    print("b")
+    # Export the trimmed audio
+    # trimmed_file_path = file_path.replace(".m4a", ".mp3")
+    trimmed_audio.export(file_path, format="mp3")
+
+    print(f"Trimmed audio saved to: {file_path}")
+    return file_path
     
 @embedding_api_router.post("/", response_model=EmbeddingResponse)
 async def convert_music_to_vector(file: UploadFile = File(...)):
@@ -22,21 +38,33 @@ async def convert_music_to_vector(file: UploadFile = File(...)):
         contents = await file.read()
         with open(full_file_path, "wb") as f:
             f.write(contents)
-        print(librosa.__version__,full_file_path)
-
+        trimmed_file_path = trim_audio(full_file_path,30)
+        print(librosa.__version__, trimmed_file_path)
+        
         # Load the audio file using librosa
-        print("ddd")
-        y, sr = librosa.load(full_file_path, sr=None)
-    
-
+        y, sr = librosa.load(trimmed_file_path, sr=None)
         # Create a Music instance (assuming Music is defined elsewhere)
         query = Music(y=y, sr=sr, music_name=file.filename, audio_url=None)  # Replace None with actual S3 URL if needed
         query.compute_music_features()
         print(query)
+        min_map = {
+        'tempo': 51.6796875,
+        'stability_score': 0.0002617532912520465,
+        'beat_strength_score': 2.1055398,
+        'dynamic_range': 0.16750057,
+        'mean_rmse': 0.05267463,
+        'mean_spectral_centroid': 1157.1957226411498
+        }
 
-        mean_map = {'tempo': 114.41952910695562, 'stability_score': 0.0003781953034425454, 'beat_strength_score': 4.0758667, 'dynamic_range': 0.3061429, 'mean_rmse': 0.11166762, 'mean_spectral_centroid': 1731.5637774109998}
-        sd_map = {'tempo': 31.647657911216665, 'stability_score': 0.00010039213540881786, 'beat_strength_score': 1.3448074, 'dynamic_range': 0.13990557, 'mean_rmse': 0.0751132, 'mean_spectral_centroid': 532.3787573483235}
-        embedder1 = MusicEmbedder(mean_map,sd_map)
+        max_map = {
+            'tempo': 184.5703125,
+            'stability_score': 0.0005519540777083322,
+            'beat_strength_score': 8.790406,
+            'dynamic_range': 0.6489084,
+            'mean_rmse': 0.31269574,
+            'mean_spectral_centroid': 2987.4354790822567
+        }
+        embedder1 = MusicEmbedder(min_map,max_map)
         query_vector = embedder1.convert_music_to_vector(query)
         print(query_vector)
         return EmbeddingResponse(embedding=query_vector)
